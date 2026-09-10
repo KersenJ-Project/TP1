d4ee09e496e703b64f02570d33db819cc4de09d6
Nyle Kersen Joseph <6290712@cegepmv.ca>
2026-09-10 15:46:57 -0400

    Relier le mouvement à l Animator

--- Assets/Scripts/MouvementRobot.cs
+++ Assets/Scripts/MouvementRobot.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class MouvementRobot : MonoBehaviour
 {
     [SerializeField] private float vitesse = 5f;
 
     private Rigidbody2D corps;
     private Vector2 direction;
-
+    private Animator animator;
     private void Awake()
     {
         corps = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -20,6 +22,9 @@ public class MouvementRobot : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         // TODO 2 : créer et normaliser le vecteur direction.
         direction = new Vector2(horizontal, vertical).normalized;
+
+        // TODO 4 : faire en sorte que le robot change d'animation.
+        animator.SetBool("EnMouvement", direction.sqrMagnitude > 0.01f);
     }
 
     private void FixedUpdate()
